4f3fa278b490abb8eacea4e29ebf31bcc886d3b2
Nikul Nayi <nayinikul97@gmail.com>
2023-06-07 16:31:10 -0400

    main Script incudes train and test model

--- main.py
+++ main.py
@@ -1,13 +1,61 @@
+import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from utils import split_data,orderTestSet
 import os
-from deepLearningModel import streetsSignModel
+from deepLearningModel import streetsSignModel, createGenerators
 
 if __name__=="__main__":
-    if False:
-        pathToData = os.path.join(os.getcwd(),'Data/Train')
-        pathToSaveTrain = os.path.join(os.getcwd(),'Data/TrainingData/Train')
-        pathToSaveVal = os.path.join(os.getcwd(),'Data/TrainingData/Val')
-        split_data(pathToData,pathToSaveTrain,pathToSaveVal)
-    pathToImages = os.path.join(os.getcwd(),'Data/Test')
-    pathToCSV = os.path.join(os.getcwd(),'Data/Test.csv')
-    orderTestSet(pathToImages,pathToCSV)
+   
+    # pathToData = os.path.join(os.getcwd(),'Data/Train')
+    # pathToSaveTrain = os.path.join(os.getcwd(),'Data/TrainingData/Train')
+    # pathToSaveVal = os.path.join(os.getcwd(),'Data/TrainingData/Val')
+    # split_data(pathToData,pathToSaveTrain,pathToSaveVal)
+
+    
+    # pathToImages = os.path.join(os.getcwd(),'Data/Test')
+    # pathToCSV = os.path.join(os.getcwd(),'Data/Test.csv')
+    # orderTestSet(pathToImages,pathToCSV)
+
+
+    pathToTrain = os.path.join(os.getcwd(),'Data/TrainingData/Train')
+    pathToVal = os.path.join(os.getcwd(),'Data/TrainingData/Val')
+    pathToTest = os.path.join(os.getcwd(),'Data/Test')
+    pathToSaveModel = 'Model/'
+    batchSize = 64
+    epochs = 15
+
+    
+    trainGenerator, valGenerator, testGenerator = createGenerators(batchSize,pathToTrain,pathToVal,pathToTest)
+    numberOfClasses = trainGenerator.num_classes
+
+    TRAIN=False
+    TEST=True
+    if TRAIN:
+        pathToSaveModel = 'Model/'
+        checkPointSaver = ModelCheckpoint(
+            pathToSaveModel,
+            monitor = 'val_accuracy',
+            mode = 'max',
+            save_best_only = True,
+            save_freq = 'epoch',
+            verbose = 1
+        )   
+
+        earlyStop = EarlyStopping(monitor='val_accuracy', patience = 10)
+
+        model = streetsSignModel(numberOfClasses)
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        model.fit(trainGenerator, 
+                epochs=epochs,
+                batch_size=batchSize,
+                validation_data=valGenerator,   
+                callbacks=[checkPointSaver, earlyStop]
+                )
+    
+    if TEST:
+        model = tf.keras.models.load_model('./Model')
+        model.summary()
+        print('Evaluating Validation Set : ')
+        model.evaluate(valGenerator)
+        print('Evaluating Test Set : ')
+        model.evaluate(testGenerator)
